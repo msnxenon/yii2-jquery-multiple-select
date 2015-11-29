c484d224d8320c510999d86fc38e5578e24e0bd6
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-11-29 09:42:59 +0300

    fix test placeholder

--- tests/MultipleSelectTest.php
+++ tests/MultipleSelectTest.php
@@ -218,6 +218,32 @@ EXPECTED_JS;
      * @param array $items
      * @dataProvider modeSelectionItemsDataProvider
      */
+    public function testWidgetPlaceholder($mode, $selection, array $items)
+    {
+        $actual = $this->getActual($mode, $selection, $items, [
+            'options' => ['placeholder' => 'some-placeholder']
+        ]);
+        list ($id, $name) = $this->getIdName($mode);
+        $selected = array_fill_keys(array_keys($items), '');
+        $selected[$selection] = ' selected';
+        $expectedHtml = <<<EXPECTED_HTML
+<select id="$id" class="form-control" name="{$name}[]" multiple size="4">
+<option value="0"$selected[0]>If you hide your ignorance, no one will hit you and you&#039;ll never learn.</option>
+<option value="1"$selected[1]>I don&#039;t talk things, sir. I talk the meaning of things.</option>
+</select>
+EXPECTED_HTML;
+        $expectedJs = <<<EXPECTED_JS
+jQuery('#$id').multipleSelect({"placeholder":"some-placeholder"});
+EXPECTED_JS;
+        $this->checkExpected($mode, $actual, $expectedHtml, $expectedJs);
+    }
+
+    /**
+     * @param int $mode
+     * @param string $selection
+     * @param array $items
+     * @dataProvider modeSelectionItemsDataProvider
+     */
     public function testWidgetReadOnlyTrue($mode, $selection, array $items)
     {
         $actual = $this->getActual($mode, $selection, $items, [
